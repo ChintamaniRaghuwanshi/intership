403d1d5cdbe45e329b0cf03be5394da7c0b9ab4d
chintamaniRaghuwanshi <chintamaniraghuwanashi@gmail.com>
2026-07-22 12:42:47 +0530

    ja

---
+++ Class practice/script.js
@@ -0,0 +1,22 @@
+let name 
+let result= "99%"
+let age=19
+let nothing= null
+console.log(typeof(name))
+console.log(typeof(age))
+console.log(typeof(result))
+console.log(typeof(nothing))
+let skill=["html","css","python","javescript","c"]
+console.log(skill[4])
+console.log(skill.length)
+let person={
+    myname:"Chintamani",
+    myage:"19",
+    isstudent:true,
+    learning:["c","python"]
+}
+console.log(person.myname)
+console.log(person["myname"])
+let mark=[90,80,70,60]
+console.log(mark)
+console.log(typeof mark)
