2a0e9eaa91f9150712a719a46f5dd2f8bdcd94ed
chintamaniRaghuwanshi <chintamaniraghuwanashi@gmail.com>
2026-07-27 13:30:58 +0530

    ss

--- Project/jscript.js
+++ Project/jscript.js
@@ -1,18 +1,16 @@
-console.log("hi")
+console.log("Javascript is connected")
 const year= new Date().getFullYear();
 document.querySelector('.site-footer p').innerHTML=
-`&copy; ${year} Chintamani Raghuwanshi. All the rights reserved.`;
-function getGreeting(){
+    `&copy;${year} Chintamani Raghuwanshi.All rights reserved.`;
+function getGreeeting(){
     const hour= new Date().getHours();
-    if(hour <12) return "Good Morning";
-    if(hour <17) return "Good Afternoon";
+    if(hour < 12) return "Good Morning";
+    if (hour< 17) return "Good Afternoon";
     return "Good Evening";
-
 }
 const heroTitle =document.querySelector('.hero-section h1');
 if (heroTitle){
-    heroTitle.textContent = `${getGreeting()}, I'm Chintamani Raghuwanshi 🤗`;
-    
+    heroTitle.textContent = `${getGreeeting()}, I'm Chintamani Raghuwanshi 🤗`;
 }
 //Dark Mode
  
